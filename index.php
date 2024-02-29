58a213f7a84cc221e839d7903d3c2e1719b7f6c8
ariannaDCD <161667841+ariannaDCD@users.noreply.github.com>
2024-02-28 18:11:31 -0800

    Se agregó persona 3

--- index.php
+++ index.php
@@ -7,10 +7,17 @@ $persona2 =[
   "nombre"=>"jesus"
   ];
 
+$persona3 =[
+  "nombre"=>"denisse"
+  ];
+
+
+
+
 $personas = [
 $persona1,
-  $persona2
-  
+  $persona2,
+  $persona3
 ];
 
 echo json_encode($personas);
